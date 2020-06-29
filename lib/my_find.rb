b9a0b98cbcda0596f0089cc6e7bc08cd4f255d4a
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 22:54:33 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,15 +1,17 @@
-require 'pry'
-
-def my_all?(collection)
-    i = 0
-    block_return_values = []
-    while i < collection.length
-      block_return_values << yield(collection[i])
-      i = i + 1
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
     end
-    if block_return_values.include?(false)
-      false
-    else
-      true
-    end
+    i = i + 1
   end
+end
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
+end
